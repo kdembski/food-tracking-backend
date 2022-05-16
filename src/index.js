b16bc9c72912c88aba246c3e06210e40a13ffe4e
Karol Dembski <krldembski@gmail.com>
2022-05-16 17:42:44 +0200

    updated listen port

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes.js"
+import router from "./routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(router)
- 
-app.listen(5000, () => console.log('Server running at http://localhost:5000'));
+app.use(router);
+
+app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
