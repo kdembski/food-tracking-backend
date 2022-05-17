0d078f144aea6a91e17cfe9e79e4085f52931afb
Karol Dembski <krldembski@gmail.com>
2022-05-17 12:56:07 +0200

    handle database disconnect

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,39 @@
 import mysql from "mysql2";
 
-const db = mysql.createConnection({
+let db_config = {
   host: "sql11.freesqldatabase.com",
   user: "sql11492636",
   password: "kDc2mfjxJv",
   database: "sql11492636",
-});
+};
 
-export default db;
+let connection;
+
+const handleDatabaseConnection = () => {
+  if (connection) {
+    connection.end();
+  }
+  connection = mysql.createConnection(db_config);
+
+  connection.connect((err) => {
+    if (err) {
+      console.log("Connecting to database error: " + err.code + "\n");
+      setTimeout(handleDatabaseConnection, 2000);
+      return;
+    }
+    console.log("Connected to database!");
+  });
+
+  connection.on("error", (err) => {
+    console.log("Error from database: " + err.code + "\n");
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      handleDatabaseConnection();
+      return;
+    }
+    throw err;
+  });
+};
+
+handleDatabaseConnection();
+
+export default connection;
