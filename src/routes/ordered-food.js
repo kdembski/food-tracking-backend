e96b6f0f42cee93c7b864cbfca1030e8c4c42833
Karol Dembski <krldembski@gmail.com>
2022-11-19 13:23:47 +0100

    get post put ordered food router

--- src/routes/ordered-food.js
+++ src/routes/ordered-food.js
@@ -15,4 +15,29 @@ orderedFoodRouter.get("/tags", (request, response) => {
     .catch((error) => response.status(400).send(error));
 });
 
+orderedFoodRouter.get("/:id", (request, response) => {
+  const id = request.params.id;
+
+  OrderedFoodController.getOrderedFoodById(id)
+    .then((results) => response.json(results))
+    .catch((error) => response.status(400).send(error));
+});
+
+orderedFoodRouter.post("/", (request, response) => {
+  const data = request.body;
+
+  OrderedFoodController.addOrderedFood(data)
+    .then((results) => response.json(results))
+    .catch((error) => response.status(400).send(error));
+});
+
+orderedFoodRouter.put("/:id", (request, response) => {
+  const id = request.params.id;
+  const data = request.body;
+
+  OrderedFoodController.updateOrderedFood(id, data)
+    .then((results) => response.json(results))
+    .catch((error) => response.status(400).send(error));
+});
+
 export default orderedFoodRouter;
