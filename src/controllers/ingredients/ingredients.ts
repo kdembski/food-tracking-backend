e519c98afc4f71b3e1ed699ebe0bce457eaf6f31
Karol Dembski <krldembski@gmail.com>
2023-07-25 16:08:57 +0200

    changed ingredients list dto to list item

--- src/controllers/ingredients/ingredients.ts
+++ src/controllers/ingredients/ingredients.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { IngredientsService } from "@/main/ingredients/services/ingredients";
-import { IngredientDTO } from "@/dtos/ingredients/ingredient";
+import {
+  IngredientDTO,
+  IngredientListItemDTO,
+} from "@/dtos/ingredients/ingredient";
 import { IngredientMapper } from "@/mappers/ingredients/ingredient";
 import { IngredientValidator } from "@/main/ingredients/validators/ingredient";
 import { ApiError } from "@/_shared/errors/models/apiError";
@@ -9,6 +12,7 @@ import { Ingredient } from "@/main/ingredients/models/ingredient";
 import { ListController } from "../_shared/list";
 import { IngredientsListFilters } from "@/types/ingredients/ingredients";
 import { ListMapper } from "@/main/_shared/list/listMapper";
+import { IngredientListItemMapper } from "@/mappers/ingredients/ingredientListItem";
 
 export class IngredientsController extends CRUDController<
   Ingredient,
@@ -20,7 +24,7 @@ export class IngredientsController extends CRUDController<
   protected validator: IngredientValidator;
   list: ListController<
     Ingredient,
-    IngredientDTO,
+    IngredientListItemDTO,
     IngredientDTO,
     IngredientsListFilters
   >;
@@ -29,7 +33,10 @@ export class IngredientsController extends CRUDController<
     service = new IngredientsService(),
     mapper = new IngredientMapper(),
     validator = new IngredientValidator(),
-    list = new ListController(service.list, new ListMapper(mapper))
+    list = new ListController(
+      service.list,
+      new ListMapper(new IngredientListItemMapper())
+    )
   ) {
     super(service, mapper, validator);
     this.service = service;
